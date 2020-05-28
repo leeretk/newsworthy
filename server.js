72aa3fa1d04752181aa314466921d15dbc2600ca
Kamy <kamy.leeret@gmail.com>
2020-05-28 05:52:48 -0600

    adding routes and html

--- server.js
+++ server.js
@@ -1,45 +1,62 @@
-// Using this template, the cheerio documentation,
-// and what you've learned in class so far, scrape a website
-// of your choice, save information from the page in a result array, and log it to the console.
+// Using the tools and techniques you learned so far,
+// you will scrape a website of your choice, then place the data
+// in a MongoDB database. Be sure to make the database and collection
+// before running this exercise.
 
-var cheerio = require("cheerio");
+// Consult the assignment files from earlier in class
+// if you need a refresher on Cheerio.
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+
+// Require axios and cheerio. This makes the scraping possible
 var axios = require("axios");
+var cheerio = require("cheerio");
 
-// Make a request via axios to grab the HTML body from the site of your choice
-axios.get("https://www.healthcarefinancenews.com/directory/supply-chain/news").then(function(response) {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(response.data);
+mongoose.connect(MONGODB_URI);
 
-  // An empty array to save the data that we'll scrape
-  var results = [];
 
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $("span.field-content").each(function(i, element) {
+// Initialize Express
+var app = express();
 
-    var title = $(element).children().text();
-    var link = $(element).find("a").attr("href");
-    
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
-    });
-  });
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
 });
 
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
 
-// <span class="field-content"><a href="/news/advocate-aurora-health-partners-premier-ramp-domestic-ppe-production">Advocate Aurora Health partners with Premier to ramp up domestic PPE production</a></span>
+// TODO: make two more routes
 
-// /* <div class="date_author">May 26 | Jeff Lagasse</div> */
+// Route 1
+// =======
+// This route will retrieve all of the data
+// from the scrapedData collection as a json (this will be populated
+// by the data you scrape using the next route)
 
-// <div class="teaser">The organizations acquired a minority stake in Prestige Ameritech, the largest domestic manufacturer of face…</div>
+// Route 2
+// =======
+// When you visit this route, the server will
+// scrape data from the site of your choice, and save it to
+// MongoDB.
+// TIP: Think back to how you pushed website data
+// into an empty array in the last class. How do you
+// push it into a MongoDB collection instead?
 
-// <img typeof="foaf:Image" src="https://www.healthcarefinancenews.com/sites/healthcarefinancenews.com/files/styles/news_thumbnail/public/coronavirus_mask_crop_2.jpg?itok=A1OnvmoF" width="190" height="110" alt=""></img> */
+/* -/-/-/-/-/-/-/-/-/-/-/-/- */
 
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
