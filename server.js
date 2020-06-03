3fe4d83961b084844bf5b3b21f57c27afe24bd23
Kamy <kamy.leeret@gmail.com>
2020-06-02 20:36:47 -0600

    scrape and server are working

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
-
-var PORT = process.env.PORT || 8080;
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 // Initialize Express
 var app = express();
@@ -11,37 +12,76 @@ var app = express();
 var databaseUrl = "scraper";
 var collections = ["scrapedData"];
 
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
-
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  // Make a request via axios to grab the HTML body from the site of your choice
+  axios.get("https://www.healthcarefinancenews.com/directory/supply-chain/news").then(function(response) {
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+  // Load the HTML into cheerio and save it to a variable
+  var $ = cheerio.load(response.data);
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+  // An empty array to save the data that we'll scrape
+  var results = [];
 
-app.use(routes);
+  // Select each element in the HTML body from which you want information.
+  $("span.field-content").each(function(i, element) {
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+    var title = $(element).children().text();
+    var link = $(element).find("a").attr("href");
+    
+  // If this found element had both a title and a link
+  if (title && link) {
+    // Insert the data in the scrapedData db
+    db.scrapedData.insert({
+      title: title,
+      link: link
+    },
+    function(err, inserted) {
+      if (err) {
+        // Log the error if one is encountered during the query
+        console.log(err);
+      }
+      else {
+        // Otherwise, log the inserted data
+        console.log(inserted);
+      }
+    });
+  }
+});
+});
+
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
