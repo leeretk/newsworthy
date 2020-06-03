93703d53f9a2c81b9f7bc0453f4e9f240382eaa6
Kamy <kamy.leeret@gmail.com>
2020-06-02 19:36:22 -0600

    adding templates

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-// Using the tools and techniques you learned so far,
-// you will scrape a website of your choice, then place the data
-// in a MongoDB database. Be sure to make the database and collection
-// before running this exercise.
-
-// Consult the assignment files from earlier in class
-// if you need a refresher on Cheerio.
-
 // Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
 
-// Require axios and cheerio. This makes the scraping possible
-var axios = require("axios");
-var cheerio = require("cheerio");
+var PORT = process.env.PORT || 8080;
 
 // Initialize Express
 var app = express();
@@ -32,63 +22,26 @@ db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.send("Hello world");
-});
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// TODO: make two more routes
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Route 1
-// =======
-// This route will retrieve all of the data
-// from the scrapedData collection as a json (this will be populated
-// by the data you scrape using the next route)
-app.get("/", function(req, res) {
-  res.send("Hello world");
-});
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Route 2
-// =======
-// When you visit this route, the server will
-// scrape data from the site of your choice, and save it to
-// MongoDB.
-// TIP: Think back to how you pushed website data
-// into an empty array in the last class. How do you
-// push it into a MongoDB collection instead?
-app.get("/all", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything
-  db.mongodb.find({}, function(err, data) {
-    // Log any errors if the server encounters one
-    if (err) {
-      console.log(err);
-    }
-    else {
-      // Otherwise, send the result of this query to the browser
-      res.json(data);
-    }
-  });
-});
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
 
-app.get("/article", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything
-  db.mongodb.find({}, function(err, data) {
-    // Log any errors if the server encounters one
-    if (err) {
-      console.log(err);
-    }
-    else {
-      // Otherwise, send the result of this query to the browser
-      res.json(data);
-    }
-  });
-});
+app.use(routes);
 
-
-/* -/-/-/-/-/-/-/-/-/-/-/-/- */
-
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
