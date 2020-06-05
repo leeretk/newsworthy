5ec857bc6fef4d392a856543e833e60ad7d43b72
Kamy <kamy.leeret@gmail.com>
2020-06-04 19:19:13 -0600

    udpates to html accordian

--- Public/app.js
+++ Public/app.js
@@ -49,7 +49,7 @@ $(document).on("click", "#savenote", function() {
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
-    url: "/articles/" + thisId,
+    url: "/articles" + thisId,
     data: {
       // Value taken from title input
       title: $("#titleinput").val(),
@@ -69,3 +69,4 @@ $(document).on("click", "#savenote", function() {
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
+
