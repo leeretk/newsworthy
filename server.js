7076e9cd7e517be2152def84854acc5b781ac938
Kamy <kamy.leeret@gmail.com>
2020-06-08 12:01:14 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ require("dotenv").config();
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
 
 // Initialize Express
 var app = express();
